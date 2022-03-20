c81ac01a68adad5e4d0d64d663f93d5dcb8fdc81
RonanPseudonym <em@emcm.lan>
2022-03-20 00:28:55 -0700

    Finish ch1

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 FLAGS = 
-FILES = memory.c chunk.c main.c
-HEADERS = memory.h chunk.h
+FILES = memory.c chunk.c debug.c value.c main.c
+HEADERS = memory.h chunk.h debug.h value.h
 OUTNAME = a.out
 
 $(OUTNAME) : $(FILES) $(HEADERS)
--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     init_chunk(&chunk);
-    write_chunk(&chunk, OP_RETURN);
+
+    int constant = add_constant(&chunk, 1.2);
+    write_chunk(&chunk, OP_CONSTANT, 0);
+    write_chunk(&chunk, constant, 0);
+
+    write_chunk(&chunk, OP_RETURN, 0);
+    disassemble_chunk(&chunk, "test chunk");
     free_chunk(&chunk);
 
     return 0;
