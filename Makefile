f7832d678f07d9d8f546aea9ec86d98742dc436c
RonanPseudonym <em@emcm.lan>
2022-03-21 00:14:02 -0700

    Finish ch2

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 FLAGS = 
-FILES = memory.c chunk.c debug.c value.c main.c
-HEADERS = memory.h chunk.h debug.h value.h
+FILES = memory.c chunk.c debug.c value.c vm.c main.c
+HEADERS = memory.h chunk.h debug.h value.h vm.h
 OUTNAME = a.out
 
 $(OUTNAME) : $(FILES) $(HEADERS)
--- main.c
+++ main.c
@@ -1,17 +1,35 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    init_vm();
+
     Chunk chunk;
     init_chunk(&chunk);
 
-    int constant = add_constant(&chunk, 1.2);
+    int constant = add_constant(&chunk, 4.2);
     write_chunk(&chunk, OP_CONSTANT, 0);
     write_chunk(&chunk, constant, 0);
 
+    constant = add_constant(&chunk, 3.4);
+    write_chunk(&chunk, OP_CONSTANT, 0);
+    write_chunk(&chunk, constant, 0);
+
+    write_chunk(&chunk, OP_ADD, 0);
+
+    constant = add_constant(&chunk, 5.6);
+    write_chunk(&chunk, OP_CONSTANT, 0);
+    write_chunk(&chunk, constant, 0);
+
+    write_chunk(&chunk, OP_DIVIDE, 0);
+    write_chunk(&chunk, OP_NEGATE, 0);
     write_chunk(&chunk, OP_RETURN, 0);
     disassemble_chunk(&chunk, "test chunk");
+    interpret(&chunk);
+
+    free_vm();
     free_chunk(&chunk);
 
     return 0;
